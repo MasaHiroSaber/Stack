6884c3cf19716b95295cbdf79cc1c3e815f0f1e2
MasaHiroSaber <1171338853@qq.com>
2023-03-01 17:00:27 +0800

    2023.3.1 17:00

--- stack.cpp
+++ stack.cpp
@@ -1,5 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "time.h"
+
 
 typedef struct Stack1 {
     int *data;
@@ -12,7 +14,7 @@ typedef struct Stack1 {
 St *CreateStack(int length) {
     St *p = (St *) malloc(sizeof(St));
     if (!p) return NULL;
-    p->data = (int *) malloc(length * sizeof(int));
+    p->data = (int *) malloc(sizeof(int));
     //p->Top = -1;
     p->Top = 0;
     p->Bottom = 0;
@@ -39,4 +41,44 @@ int GetTop(St *stack, int *value) {
     int index = stack->Top - 1;
     (*value) = stack->data[index];
     return 1;
+}
+
+void OutPut(St *stack) {
+    int length = (stack->Top - stack->Bottom);
+    for (int i = 0; i < length; i++) {                 // 0 1 2 3 4 5
+        int value = stack->data[length - i - 1];
+        printf("\tPosition:%d Value:%d\n", length - i - 1, value);
+    }
+}
+
+int main() {
+    srand(time(0));
+#define LENGTH 10
+    St *stack = CreateStack(LENGTH);
+    for (auto i = 0; i < LENGTH; i++) {
+        int op = rand() % 4;
+        int value = 1 + rand() % 10;
+        int *getvalue = NULL;
+        switch (op) {
+            case 0:
+            case 1:
+            case 2:
+            case 3: {
+                int code = Push(stack, value);
+                if (code == 0) printf("ERROR\n");
+                else printf("SUCCESS PUSH\n");
+                break;
+            }
+
+//            case 3: {
+//                int code = Pop(stack, getvalue);
+//                if (code == 0) printf("ERROR\n");
+//                else printf("SUCCESS POP\n");
+//            }
+
+        }
+        OutPut(stack);
+    }
+    free(stack);
+    return 0;
 }
