f086e36b6c647106f390475775059c33eeba149e
MasaHiroSaber <1171338853@qq.com>
2023-03-02 12:59:58 +0800

    2023.3.2 12:59

--- stack.cpp
+++ stack.cpp
@@ -3,7 +3,8 @@
 #include "time.h"
 
 
-typedef struct Stack1 {
+typedef struct Stack1
+{
     int *data;
     int Top;
     int Bottom;
@@ -11,7 +12,8 @@ typedef struct Stack1 {
 } St;
 
 
-St *CreateStack(int length) {
+St *CreateStack(int length)
+{
     St *p = (St *) malloc(sizeof(St));
     if (!p) return NULL;
     p->data = (int *) malloc(sizeof(int));
@@ -22,7 +24,8 @@ St *CreateStack(int length) {
     return p;
 }
 
-int Push(St *stack, int value) {
+int Push(St *stack, int value)
+{
     if (((stack->Top) - (stack->Bottom)) == (stack->Size) - 1)
         return 0;
     //stack -> data[++stack->Top] = value;
@@ -30,40 +33,48 @@ int Push(St *stack, int value) {
     return 1;
 }
 
-int Pop(St *stack, int *value) {
+int Pop(St *stack, int *value)
+{
     if (stack->Top == stack->Bottom) return 0;
     (*value) = stack->data[--stack->Top];
     return 1;
 }
 
-int GetTop(St *stack, int *value) {
+int GetTop(St *stack, int *value)
+{
     if (stack->Top == stack->Bottom) return 0;
     int index = stack->Top - 1;
     (*value) = stack->data[index];
     return 1;
 }
 
-void OutPut(St *stack) {
+void OutPut(St *stack)
+{
     int length = (stack->Top - stack->Bottom);
-    for (int i = 0; i < length; i++) {                 // 0 1 2 3 4 5
+    for (int i = 0; i < length; i++)
+    {                 // 0 1 2 3 4 5
         int value = stack->data[length - i - 1];
         printf("\tPosition:%d Value:%d\n", length - i - 1, value);
     }
 }
 
-int main() {
+int main()
+{
     srand(time(0));
 #define LENGTH 10
     St *stack = CreateStack(LENGTH);
-    for (auto i = 0; i < LENGTH; i++) {
+    for (auto i = 0; i < LENGTH; i++)
+    {
         int op = rand() % 4;
         int value = 1 + rand() % 10;
         int *getvalue = NULL;
-        switch (op) {
+        switch (op)
+        {
             case 0:
             case 1:
             case 2:
-            case 3: {
+            case 3:
+            {
                 int code = Push(stack, value);
                 if (code == 0) printf("ERROR\n");
                 else printf("SUCCESS PUSH\n");
